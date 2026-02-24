49cab48d2e7ee751528342562faee23321cd03e2
HypeCloud Macbook <apple@Mac.lan>
2026-02-24 21:12:04 +0700

    feat: initial commit - Day 5

--- bai_1.js
+++ bai_1.js
@@ -1,9 +1,8 @@
 let i = 10;
 while (i >= 0) {
-   console.log(i);
 if (i === 5) {
     console.log("Đã qua nửa thời gian");
-} else if (i !== 5 && i > 0) {
+} else {
     console.log("Còn lại " + i + " giây");
 }
 i--;
